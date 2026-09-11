3c258db71380fb5773c131a2dcddc09d4b3d7bb7
Esdras L. Bispo Jr. <esdraspiano@gmail.com>
2026-09-11 16:27:50 -0300

    refactor: remover adições desnecessárias de colunas nas migrações de eventos

--- migrations/20260416092527-add-url-template-base-to-eventos.js
+++ migrations/20260416092527-add-url-template-base-to-eventos.js
@@ -3,10 +3,6 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('eventos', 'url_template_base', {
-      type: Sequelize.STRING,
-      allowNull: true,
-    })
     const tableDesc = await queryInterface.describeTable('eventos')
     if (!tableDesc.url_template_base) {
       await queryInterface.addColumn('eventos', 'url_template_base', {
@@ -17,7 +13,6 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.removeColumn('eventos', 'url_template_base')
     const tableDesc = await queryInterface.describeTable('eventos')
     if (tableDesc.url_template_base) {
       await queryInterface.removeColumn('eventos', 'url_template_base')
